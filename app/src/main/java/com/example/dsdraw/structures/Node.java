2870a6c88e6dfc4fb6e8500cfa22bb51f2b2f5cd
jai1396 <jai1396@gmail.com>
2021-11-22 02:52:14 -0500

    add prompt and selected to tree node

--- app/src/main/java/com/example/dsdraw/structures/Node.java
+++ app/src/main/java/com/example/dsdraw/structures/Node.java
@@ -5,15 +5,34 @@ public class Node
     public char label;
     public Node left, right;
     private CanvasPoint point;
+    private boolean selected;
+    private boolean prompt;
 
     public Node(char _label)
     {
         label = _label;
         left = right = null;
+        selected = prompt = false;
     }
 
     public CanvasPoint getPoint() { return point; }
     public void setPoint(CanvasPoint pt) {
         point = pt;
+    }
+
+    public boolean isPrompt() {
+        return prompt;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setPrompt(boolean prompt) {
+        this.prompt = prompt;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
     }
 }
